7cca8b9beb0260bb6a1251e5f292c8d2d1d79c08
leggebroten <lee_eggebroten@live.com>
2024-02-11 16:25:10 -0700

    Removed unneeded `deps` options

--- mix.exs
+++ mix.exs
@@ -41,10 +41,10 @@ defmodule TypedStructCtor.MixProject do
 
   defp deps do
     [
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test]},
+      {:dialyxir, "~> 1.3", only: [:dev, :test]},
       {:ecto, "~> 3.10"},
-      {:ex_doc, "~> 0.30", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.30", only: :dev},
       {:typedstruct, "~> 0.5.2"},
       {:typed_struct_ecto_changeset, "~> 1.0.0"}
     ]
